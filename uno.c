514d5bd93324e2ca9d13eb0066921b4f3004425c
fierychicken65 <fadiltutu@gmail.com>
2023-10-16 22:19:44 +0530

    added all structures

--- uno.c
+++ uno.c
@@ -1,3 +1,31 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+//Main deck containing all the cards
+struct  DeckStack
+{
+    char num;
+    char color;
+    struct Deckstack * next;
+};
+
+//Stack to Deposit cards
+struct DepStack
+{
+    char num;
+    char color;
+    struct DepStack * next;   
+};
+
+//Player 1
+struct Player{
+    char num;
+    char color;
+    struct Player * next;   
+};  
+
+struct Computer{
+    char num;
+    char color;
+    struct Computer * next;   
+};
